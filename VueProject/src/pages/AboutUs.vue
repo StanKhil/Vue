fae2f1a739809b4c806f858521037f1b6fa1e79a
pln1 <polonskiy2008alex@gmail.com>
2024-10-06 19:34:53 +0300

    sahaaa goool

--- VueProject/src/pages/AboutUs.vue
+++ VueProject/src/pages/AboutUs.vue
@@ -9,7 +9,7 @@ function goTo(route) {
 
 <template>
     <div class="header">
-      <h1 class="title">About Us</h1>
+      <h1 class="title">About Us sahhaaa</h1>
       <div id="pages">
         <button @click="goTo('/')">Home</button>
         <button @click="goTo('/about')">About Us</button>
