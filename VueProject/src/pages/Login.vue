d84f805e835439fd1526991ef11cc27435471b6b
Stanislav <stankhil@gmail.com>
2024-10-07 09:22:26 +0300

    login

---
+++ VueProject/src/pages/Login.vue
@@ -0,0 +1,45 @@
+<script setup>
+import {ref,reactive} from 'vue'
+let login=ref("")
+let password=ref("")
+
+
+
+function EnterData(){
+    console.log(login.value + " " + password.value)
+}
+</script>
+
+<template>
+    <div class="head">
+        <h1>Login</h1>
+    </div>
+    <div class="main">
+        <input v-model="login" placeholder="Enter your login">
+        <br>
+        <input v-model="password" placeholder="Enter your password" type="password">
+        <br>
+        <button @click="EnterData">Enter</button>
+    </div>
+</template>
+
+<style scoped>
+.main{
+    border: 0px;
+}
+.head{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color:#5083cf;
+    color:white;
+}
+input{
+    padding: 10px;
+    padding-left: 80px;
+    margin: 15px 200px;
+}
+button{
+    margin: 15px 300px;
+}
+</style>
--- VueProject/src/router.js
+++ VueProject/src/router.js
@@ -4,12 +4,14 @@ import Home from './pages/Home.vue'
 import AboutUs from './pages/AboutUs.vue'
 import Groups from './pages/Groups.vue'
 import Profile from './pages/Profile.vue'
+import Login from './pages/Login.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: AboutUs },
   { path: '/groups', component: Groups },
   { path: '/profile', component: Profile },
+  { path: '/login', component: Login },
 ]
 
 const router = createRouter({
